8bd38feaf43a1fad1c36cbfd39dca21fbc12325a
Kaveesha6 <161047331+Kaveesha6@users.noreply.github.com>
2024-02-24 09:54:51 +0530

    Add files via upload

---
+++ Inky Ponky/Inky.java
@@ -0,0 +1,29 @@
+public class Inky{
+
+public Inky(String Pinky){
+
+	System.out.println(Pinky);
+
+}
+
+public Inky(String Pinky, String Blinky, String Rinky){
+	
+	System.out.println(Blinky+ " " +Pinky);
+	System.out.println(Pinky+ " " +Rinky);
+	System.out.println(Blinky+ " " +Pinky+ " " +Rinky);
+
+}
+
+public static void main(String args[]){
+
+String Pinky = "Donkey";
+String Blinky = "Pinky";
+String Rinky = "Monkey";
+
+Inky Ponky = new Inky(Pinky);
+
+Inky Ponky2 = new Inky(Pinky, Blinky, Rinky);
+
+}
+
+}
